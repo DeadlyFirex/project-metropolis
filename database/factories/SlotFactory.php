bc9c072fedeca1e45f8061230094b619e5e74741
Deadly <info.saddiki@gmail.com>
2025-04-16 15:24:06 +0200

    Added docstring to explain factory contraints

--- database/factories/SlotFactory.php
+++ database/factories/SlotFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Slot;
-use App\Models\Configuration;
 use App\Models\Module;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -13,6 +12,13 @@ class SlotFactory extends Factory
 
     public function definition(): array
     {
+        /*
+         * The generation is based on the following assumptions:
+         * - The timetable is a 3x4 grid (3 rows and 4 columns).
+         * - Each slot is represented by a row and column index that is unique.
+         * This may create a conflict if the same row and column are generated for different slots.
+         * TODO: Implement a mechanism to ensure that each slot has a unique row and column combination.
+         */
         return [
             'row' => $this->faker->numberBetween(0, 2),
             'column' => $this->faker->numberBetween(0, 3),
