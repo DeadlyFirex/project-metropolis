21fdd4f45d9c481629db14affe3d15ebeb8ec362
Loïc van Zon <l.vanzon5@student.avans.nl>
2025-04-23 12:17:30 +0200

    made realistic testdata, images now work much easier see readme for how to

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -3,12 +3,79 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Module;
 
 class ModuleSeeder extends Seeder
 {
-    public function run(): void
+    public function run()
     {
-        Module::factory()->count(10)->create();
+        $modules = [
+            [
+                'name' => 'Ziekenhuis',
+                'description' => 'Een instelling voor medische zorg, behandeling en operaties.',
+                'category' => 'zorg',
+                'image' => 'ziekenhuis.jpg',
+                'factors' => [
+                    'gezondheid' => 0.95,
+                    'happiness' => 0.4,
+                    'mentale_rust' => 0.3,
+                    'sociale_interactie' => 0.6,
+                ],
+            ],
+            [
+                'name' => 'Woonhuis',
+                'description' => 'Een fijne plek om thuis te komen en tot rust te komen.',
+                'category' => 'residentieel',
+                'image' => 'huis.jpg',
+                'factors' => [
+                    'gezondheid' => 0.7,
+                    'happiness' => 0.9,
+                    'mentale_rust' => 0.85,
+                    'sociale_interactie' => 0.6,
+                ],
+            ],
+            [
+                'name' => 'Stadspark',
+                'description' => 'Een openbaar park voor ontspanning, natuurbeleving en beweging.',
+                'category' => 'openbaar',
+                'image' => 'park.jpg',
+                'factors' => [
+                    'gezondheid' => 0.8,
+                    'happiness' => 0.95,
+                    'mentale_rust' => 0.9,
+                    'sociale_interactie' => 0.7,
+                ],
+            ],
+            [
+                'name' => 'Basisschool',
+                'description' => 'Een leeromgeving die ontwikkeling en sociale groei bevordert.',
+                'category' => 'onderwijs',
+                'image' => 'school.jpg',
+                'factors' => [
+                    'gezondheid' => 0.6,
+                    'happiness' => 0.7,
+                    'mentale_rust' => 0.5,
+                    'sociale_interactie' => 0.95,
+                ],
+            ],
+        ];
+
+        foreach ($modules as $mod) {
+            $imagePath = 'modules/' . $mod['image'];
+            $sourceImage = database_path('seeders/images/' . $mod['image']);
+
+            if (file_exists($sourceImage)) {
+                Storage::disk('public')->putFileAs('modules', new \Illuminate\Http\File($sourceImage), $mod['image']);
+            }
+
+            Module::create([
+                'name' => $mod['name'],
+                'description' => $mod['description'],
+                'category' => $mod['category'],
+                'image_path' => $imagePath,
+                'factors' => json_encode($mod['factors'], JSON_THROW_ON_ERROR),
+            ]);
+        }
     }
 }
