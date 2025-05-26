3058b991e5907a1a5541ea27cae9806f8d98c197
BramDekkersJojo <brammetjed@gmail.com>
2025-05-26 11:51:42 +0200

    added increase button to condition dashboard

--- resources/views/conditions_dashboard.blade.php
+++ resources/views/conditions_dashboard.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="font-semibold text-xl text-gray-800 leading-tight">Conditions&nbsp;Dashboard</h1>
+        <button class="bg-blue-500 text-white px-6 py-3 text-xl rounded w-auto" id="increaseFontBtn">
+            Increase Font Size
+        </button>
     </x-slot>
 
     <style>[x-cloak]{display:none!important;}</style>
