9c2e6befc4418d523b06fd89329684c1afc7ff78
Loïc van Zon <l.vanzon5@student.avans.nl>
2025-06-23 14:37:55 +0200

    fixed app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,18 @@
-import './bootstrap';
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
+
+import './dashboard.js';
+import './font-size.js';
+import './effect-control.js';
+import './effect-flash.js';
+import './openModule.js';
+import './services/feedback.js';
+
+import { initLibrarySearch } from './services/library.js';
+import { initFeedback } from './services/feedback.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initLibrarySearch();
+    initFeedback();
+});
